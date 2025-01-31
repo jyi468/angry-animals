49b8b07351c77e417c1012650d1666b9a7a7619f
jyi468 <jyi468@gmail.com>
2025-01-30 20:03:26 -0500

    chore: add drag and release logic

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -1,6 +1,13 @@
 extends RigidBody2D
 
 
+enum ANIMAL_STATE { READY, DRAG, RELEASE }
+
+@onready var label: Label = $Label
+
+
+var _state: ANIMAL_STATE = ANIMAL_STATE.READY
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,7 +15,37 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pass
+	update(delta)
+	label.text = "%s" % ANIMAL_STATE.keys()[_state]
+
+
+func set_new_state(new_state: ANIMAL_STATE) -> void:
+	_state = new_state
+	if _state == ANIMAL_STATE.RELEASE:
+		freeze = false
+	elif _state == ANIMAL_STATE.DRAG:
+		pass
+
+	
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released("drag") == true:
+			set_new_state(ANIMAL_STATE.RELEASE)
+			return true
+	return false
+	
+	
+func update_drag() -> void:
+	if detect_release() == true:
+		return
+	var gmp: Vector2 = get_global_mouse_position()
+	position = gmp
+	
+	
+func update(delta: float) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG:
+			update_drag()
 
 
 func die() -> void:
@@ -18,3 +55,8 @@ func die() -> void:
 
 func _on_screen_exited() -> void:
 	die()
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if _state == ANIMAL_STATE.READY and event.is_action_pressed("drag"):
+		set_new_state(ANIMAL_STATE.DRAG)
