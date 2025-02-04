0feef1f62b191b51f547c4da39cb0a3ed439f50d
jyi468 <jyi468@gmail.com>
2025-02-03 19:45:39 -0500

    chore: update dragging action to prevent shift

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -3,6 +3,16 @@ extends RigidBody2D
 
 enum ANIMAL_STATE { READY, DRAG, RELEASE }
 
+
+const DRAG_LIM_MAX: Vector2 = Vector2(0, 60)
+const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
+
+
+var _start: Vector2 = Vector2.ZERO
+var _drag_start: Vector2 = Vector2.ZERO
+var _dragged_vector: Vector2 = Vector2.ZERO
+
+
 @onready var label: Label = $Label
 
 
@@ -10,13 +20,14 @@ var _state: ANIMAL_STATE = ANIMAL_STATE.READY
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_start = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	update(delta)
-	label.text = "%s" % ANIMAL_STATE.keys()[_state]
+	label.text = "%s\n" % ANIMAL_STATE.keys()[_state]
+	label.text += "%.1f,%.1f" %[_dragged_vector.x, _dragged_vector.y]
 
 
 func set_new_state(new_state: ANIMAL_STATE) -> void:
@@ -24,8 +35,28 @@ func set_new_state(new_state: ANIMAL_STATE) -> void:
 	if _state == ANIMAL_STATE.RELEASE:
 		freeze = false
 	elif _state == ANIMAL_STATE.DRAG:
-		pass
+		_drag_start = get_global_mouse_position()
 
+	
+func get_dragged_vector(gmp: Vector2) -> Vector2:
+	return gmp - _drag_start	
+	
+	
+func drag_in_limits() -> void:
+	_dragged_vector.x = clampf(
+		_dragged_vector.x,
+		DRAG_LIM_MIN.x,
+		DRAG_LIM_MAX.x
+	)
+	
+	_dragged_vector.y = clampf(
+		_dragged_vector.y,
+		DRAG_LIM_MIN.y,
+		DRAG_LIM_MAX.y
+	)
+	
+	position = _start + _dragged_vector
+	
 	
 func detect_release() -> bool:
 	if _state == ANIMAL_STATE.DRAG:
@@ -38,8 +69,12 @@ func detect_release() -> bool:
 func update_drag() -> void:
 	if detect_release() == true:
 		return
+	
 	var gmp: Vector2 = get_global_mouse_position()
-	position = gmp
+	_dragged_vector = get_dragged_vector(gmp)
+	drag_in_limits()
+	# Moves bird to mouse position exactly (shifts)
+	# position = gmp
 	
 	
 func update(delta: float) -> void:
